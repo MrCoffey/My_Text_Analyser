cb46eb2126231afa1f5664d01c377c242c0c3a4c
Jorge Madrid <jorjilio@hotmail.com>
2014-10-15 11:53:07 -0500

    Adding file to scan

---
+++ analizer.rb
@@ -0,0 +1,20 @@
+lines = File.readlines("text.txt")
+line_count = lines.size
+text = lines.join
+
+puts "#{line_count} lines"
+
+
+=begin
+*Original code to open a .txt file and analyze it
+
+text = ''
+line_count = 0
+File.open("text.txt").each do |line|
+	line_count += 1
+	text << line
+end	 
+
+puts "#{line_count} lines"
+
+=end
