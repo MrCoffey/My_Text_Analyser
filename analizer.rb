6e979da51be0b946cb4f1c5aa21a7aafcfe9ddc9
Jorge Madrid <jorjilio@hotmail.com>
2014-10-15 12:28:00 -0500

    Script counts character, words, sentences ands paragraphs

--- analizer.rb
+++ analizer.rb
@@ -1,10 +1,3 @@
-lines = File.readlines("text.txt")
-line_count = lines.size
-text = lines.join
-
-puts "#{line_count} lines"
-
-
 =begin
 *Original code to open a .txt file and analyze it
 
@@ -18,3 +11,31 @@ end
 puts "#{line_count} lines"
 
 =end
+
+#Opening file and counting lines 
+lines = File.readlines("text.txt")
+line_count = lines.size
+text = lines.join
+
+puts "#{line_count} lines"
+
+#Counting characters
+total_characters = text.length
+puts "#{total_characters} characters"
+
+#Eradicating blank spaces
+total_characters_nospaces = text.gsub(/\s+/, '').length
+puts "#{total_characters_nospaces} characters excluding spaces"
+
+#counting words
+word_count = text.split.length
+puts "#{word_count} words"
+
+#Counting Sentences and Paragraphs (pipes split the three characters)
+paragraph_count = text.split(/\n\n/).length
+puts "#{paragraph_count} paragraphs"
+
+sentence_count = text.split(/\.|\?|!/).length
+puts "#{sentence_count} sentences"
+
+#Calculating Averages
